7cdbe117b15c222dbe4906baa6a979ab612949c2
Thallys Silva <thallysfreed@gmail.com>
2020-11-11 01:23:56 -0300

    Criando testes para UserController

---
+++ src/main/java/com/silth/wallet/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.silth.wallet.controller;
+
+import javax.validation.Valid;
+
+import com.silth.wallet.dto.UserDTO;
+import com.silth.wallet.entity.User;
+import com.silth.wallet.response.Response;
+import com.silth.wallet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto, BindingResult result){
+        Response<UserDTO> response = new Response<UserDTO>();
+
+        User user = userService.save(this.convertDtoToEntity(dto));
+
+        response.setData(this.convertEntityToDto(user));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    private User convertDtoToEntity(UserDTO dto){
+        User u = new User();
+        u.setPassword(dto.getPassword());
+        u.setName(dto.getName());
+        u.setEmail(dto.getEmail());
+
+        return u;
+    }
+
+    private UserDTO convertEntityToDto(User user){
+        UserDTO u = new UserDTO();
+        u.setPassword(user.getPassword());
+        u.setName(user.getName());
+        u.setEmail(user.getEmail());
+
+        return u;
+    }
+}
---
+++ src/test/java/com/silth/wallet/controller/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.silth.wallet.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.silth.wallet.dto.UserDTO;
+import com.silth.wallet.entity.User;
+import com.silth.wallet.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    private static final String EMAIL = "email@teste.com";
+    private static final String NAME = "User User";
+    private static final String PASSWORD = "12456";
+    private static final String URL = "/user";
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    public void testSave() throws Exception {
+        BDDMockito.given(userService.save(Mockito.any(User.class))).willReturn(getMockUser());
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    public User getMockUser() {
+        User u = new User();
+        u.setEmail(EMAIL);
+        u.setName(NAME);
+        u.setPassword(PASSWORD);
+
+        return u;
+    }
+
+    public String getJsonPayload() throws JsonProcessingException {
+        UserDTO dto = new UserDTO();
+        dto.setEmail(EMAIL);
+        dto.setName(NAME);
+        dto.setPassword(PASSWORD);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.writeValueAsString(dto);
+    }
+}
