0c6ee78a076ed6037d058e5a75aba3d601d4d0e6
Thallys Silva <thallysfreed@gmail.com>
2021-01-14 23:54:31 -0300

    Criação de testes e camadas para WalletItemRepository

--- src/main/java/com/silth/wallet/repository/WalletItemRepository.java
+++ src/main/java/com/silth/wallet/repository/WalletItemRepository.java
@@ -1,7 +1,21 @@
 package com.silth.wallet.repository;
 
 import com.silth.wallet.entity.WalletItem;
+import com.silth.wallet.util.enums.TypeEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 public interface WalletItemRepository extends JpaRepository<WalletItem, Long> {
+    Page<WalletItem> findAllByWalletIdAndDateGreaterThanEqualAndDateLessThanEqual(Long wallet, Date init, Date end, Pageable page);
+
+    List<WalletItem> findByWalletIdAndType(Long wallet, TypeEnum type);
+
+    @Query(value = "select sum(value) from WalletItem wi where wi.wallet.id = :id")
+    BigDecimal sumByWalletId(Long id);
 }
--- src/test/java/com/silth/wallet/repository/WalletItemRepositoryTest.java
+++ src/test/java/com/silth/wallet/repository/WalletItemRepositoryTest.java
@@ -11,15 +11,25 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WalletItemRepositoryTest {
+
     private static final Date DATE = new Date();
     private static final TypeEnum TYPE = TypeEnum.EN;
     private static final String DESCRIPTION = "Conta de Internet";
@@ -28,52 +38,130 @@ public class WalletItemRepositoryTest {
     private Long idWalletItemSaved;
 
     @Autowired
-    WalletItemRepository walletItemRepository;
-
+    WalletItemRepository repository;
     @Autowired
     WalletRepository walletRepository;
 
     @Before
-    public void setup(){
-        Wallet wallet =new Wallet();
-        wallet.setName("Carteira 1");
-        wallet.setValue(BigDecimal.valueOf(500));
+    public void setUp() {
+        Wallet w = new Wallet();
+        w.setName("Carteira Teste");
+        w.setValue(BigDecimal.valueOf(250));
+        walletRepository.save(w);
 
-        idWalletSaved = walletRepository.save(wallet).getId();
+        WalletItem wi = new WalletItem(null, w, DATE, TYPE, DESCRIPTION, VALUE);
+        repository.save(wi);
 
-        WalletItem walletItem = new WalletItem(1L, wallet, DATE, TYPE, DESCRIPTION, VALUE);
-
-        idWalletItemSaved = walletItemRepository.save(walletItem).getId();
+        idWalletItemSaved = wi.getId();
+        idWalletSaved = w.getId();
     }
 
     @After
-    public void tearDown(){
-        walletItemRepository.deleteAll();
+    public void tearDown() {
+        repository.deleteAll();
         walletRepository.deleteAll();
     }
 
     @Test
-    public void testSave(){
-        Wallet wallet =new Wallet();
-        wallet.setName("Carteira 1");
-        wallet.setValue(BigDecimal.valueOf(500));
+    public void testSave() {
 
-        walletRepository.save(wallet);
+        Wallet w = new Wallet();
+        w.setName("Carteira teste");
+        w.setValue(BigDecimal.valueOf(500));
+        walletRepository.save(w);
 
-        WalletItem walletItem = new WalletItem(1L, wallet, DATE, TYPE, DESCRIPTION, VALUE);
+        WalletItem wi = new WalletItem(1L, w, DATE, TYPE, DESCRIPTION, VALUE);
 
-        WalletItem response = walletItemRepository.save(walletItem);
+        WalletItem response = repository.save(wi);
 
-        Assert.assertNotNull(response);
-        Assert.assertEquals(DATE, response.getDate());
-        Assert.assertEquals(TYPE, response.getType());
-        Assert.assertEquals(DESCRIPTION, response.getDescription());
-        Assert.assertEquals(VALUE, response.getValue());
+        assertNotNull(response);
+        assertEquals(response.getDescription(), DESCRIPTION);
+        assertEquals(response.getType(), TYPE);
+        assertEquals(response.getValue(), VALUE);
+        assertEquals(response.getWallet().getId(), w.getId());
+
     }
 
-    @Test(expected = ConstraintViolationException.class)
-    public void testarException(){
-        WalletItem walletItem = new WalletItem(null, null, DATE, null, DESCRIPTION, null);
-        walletItemRepository.save(walletItem);
+    @Test
+    public void testUpdate() {
+        Optional<WalletItem> wi = repository.findById(idWalletItemSaved);
+
+        String description = "Nova descrição";
+
+        WalletItem changed = wi.get();
+        changed.setDescription(description);
+
+        repository.save(changed);
+
+        Optional<WalletItem> newWalletItem = repository.findById(idWalletItemSaved);
+
+        assertEquals(description, newWalletItem.get().getDescription());
+    }
+
+    @Test
+    public void deleteWalletItem() {
+        Optional<Wallet> wallet = walletRepository.findById(idWalletSaved);
+        WalletItem wi = new WalletItem(null, wallet.get(), DATE, TYPE, DESCRIPTION, VALUE);
+
+        repository.save(wi);
+
+        repository.deleteById(wi.getId());
+
+        Optional<WalletItem> response = repository.findById(wi.getId());
+
+        assertFalse(response.isPresent());
+    }
+
+    @Test
+    public void testFindBetweenDates() {
+        Optional<Wallet> w = walletRepository.findById(idWalletSaved);
+
+        LocalDateTime localDateTime = DATE.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        Date currentDatePlusFiveDays = Date.from(localDateTime.plusDays(5).atZone(ZoneId.systemDefault()).toInstant());
+        Date currentDatePlusSevenDays = Date.from(localDateTime.plusDays(7).atZone(ZoneId.systemDefault()).toInstant());
+
+
+        repository.save(new WalletItem(null, w.get(), currentDatePlusFiveDays, TYPE, DESCRIPTION, VALUE));
+        repository.save(new WalletItem(null, w.get(), currentDatePlusSevenDays, TYPE, DESCRIPTION, VALUE));
+
+        PageRequest pg = PageRequest.of(0, 10);
+        Page<WalletItem> response = repository.findAllByWalletIdAndDateGreaterThanEqualAndDateLessThanEqual(idWalletSaved, DATE, currentDatePlusFiveDays, pg);
+
+        assertEquals(response.getContent().size(), 2);
+        assertEquals(response.getTotalElements(), 2);
+        assertEquals(response.getContent().get(0).getWallet().getId(), idWalletSaved);
+    }
+
+    @Test
+    public void testFindByType() {
+        List<WalletItem> response = repository.findByWalletIdAndType(idWalletSaved, TYPE);
+
+        assertEquals(response.size(), 1);
+        assertEquals(response.get(0).getType(), TYPE);
+    }
+
+    @Test
+    public void testFindByTypeSd() {
+
+        Optional<Wallet> w = walletRepository.findById(idWalletSaved);
+
+        repository.save(new WalletItem(null, w.get(), DATE, TypeEnum.SD, DESCRIPTION, VALUE));
+
+        List<WalletItem> response = repository.findByWalletIdAndType(idWalletSaved, TypeEnum.SD);
+
+        assertEquals(response.size(), 1);
+        assertEquals(response.get(0).getType(), TypeEnum.SD);
+    }
+
+    @Test
+    public void testSumByWallet() {
+        Optional<Wallet> w = walletRepository.findById(idWalletSaved);
+
+        repository.save(new WalletItem(null, w.get(), DATE, TYPE, DESCRIPTION, BigDecimal.valueOf(150)));
+
+        BigDecimal response = repository.sumByWalletId(idWalletSaved);
+
+        assertEquals(BigDecimal.valueOf(285.0).compareTo(response), 0);
     }
 }
